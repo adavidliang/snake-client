f6d7d17a6b76ab8e264e26b6ccc8fdee28ebe5be
adavidliang <adavidliang@gmail.com>
2023-06-07 19:02:45 -0400

    setInterval to control the snake

--- client.js
+++ client.js
@@ -1,22 +1,37 @@
 const net = require("net");
+let moveInterval;
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: "50541",
+    
   });
 
   conn.on("event name", () => {
 
   });
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: JL");
+    //  moveInterval = setInterval(() => {
+    //   conn.write("Move: up"); // Replace "up" with the desired direction
+    // }, 500);
+  })
+    conn.on('end', () => {
+      clearInterval(moveInterval)
+    })
+
+
+    // conn.write("Move: right");
+        // conn.write("Move: up");
+    // conn.write("Move: left");
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
 
-  conn.on("connect", () => {
-    conn.write("Hello from client!");
-  });
+
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
